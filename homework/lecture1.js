92adc1b62209dc42e07ce340a2445bd0eba7a322
Larisataranova <110540356+Larisataranova@users.noreply.github.com>
2022-12-05 17:41:00 +0400

    Лекция 1 - 3 модуль

---
+++ homework/lecture1.js
@@ -0,0 +1,20 @@
+var myName = 'Larisa';
+var myAge = 41;
+var iHaveChildren = true;
+var mySon1 = 'Alexander';
+var mySon2 = 'Gleb';
+var myPet = 'turtle';
+var iHaveDog = false;
+var myHeight = 170;
+
+// Number
+
+console.log(Number(myName), Number(myAge), Number(iHaveChildren), Number(mySon2), Number(mySon1), Number(myPet), Number(iHaveDog), Number(myHeight) );
+
+// String
+
+console.log(String(myName), String(myAge), String(iHaveChildren), String(mySon2), String(mySon1), String(myPet), String(iHaveDog), String(myHeight) );
+
+// Boolean
+
+console.log(Boolean(myName), Boolean(myAge), Boolean(iHaveChildren), Boolean(mySon2), Boolean(mySon1), Boolean(myPet), Boolean(iHaveDog), Boolean(myHeight) );
---
+++ homework/lecture2.js
@@ -0,0 +1,16 @@
+let userNumber = prompt();
+
+for ( let i = 1; i <= userNumber; i++) {
+    if (i % 4 === 0) continue;
+    console.log (i)
+}
+
+let i = 1;
+
+while ( userNumber ) {
+    if ( i === 
+        userNumber) 
+    break;
+    console.log (userNumber)
+    i++
+}
